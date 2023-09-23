5e5cf4188fff51e8f8ef8e1e16cbf455b1d352df
lanrewadge <sobolaudg@gmail.com>
2023-09-23 09:52:05 +0100

    ok

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return '''
+        <form action="/greet" method="post">
+            <label for="name">Enter your name:</label>
+            <input type="text" id="name" name="name"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    '''
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
